24892e3a5e3cd6e089fc5f9cf0f71658119d6845
Anthonius Munthi <toni.munthi@gmail.com>
2012-12-11 00:04:26 +0800

    fix core autoloading

--- autoload.php
+++ autoload.php
@@ -1,7 +1,15 @@
 <?php
-$loader = require_once __DIR__.'/vendor/autoload.php';
-$loader->register();
+/*
+ * This file is part of the dayax project.
+ *
+ * (c) Anthonius Munthi <toni.dayax@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-use dayax\core\Exception;
-Exception::registerAutoload();
+$loader = require_once __DIR__ . '/vendor/autoload.php';
+$loader->register();
+dayax\core\Dayax::setLoader($loader);
+dayax\core\Exception::registerAutoload();
 ?>
